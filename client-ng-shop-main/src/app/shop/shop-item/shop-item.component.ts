b421d0a9dda45a65258fe870c701210672d3f4bb
Malik <Malik@MALIK-X>
2025-09-10 17:40:48 +0300

    fix: Images 8

--- client-ng-shop-main/src/app/shop/shop-item/shop-item.component.ts
+++ client-ng-shop-main/src/app/shop/shop-item/shop-item.component.ts
@@ -1,32 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IProduct } from '../../shared/Models/Product';
 import { BasketService } from '../../basket/basket.service';
 import { FavoriteService } from '../../favorite/favorite.service';
-import { Product } from '../../shared/Models/Favorites';
 
 @Component({
   selector: 'app-shop-item',
   templateUrl: './shop-item.component.html',
-  styleUrl: './shop-item.component.scss',
+  styleUrls: ['./shop-item.component.scss']
 })
-export class ShopItemComponent {
-  constructor(private _service: BasketService ,private favoriteService: FavoriteService) {}
+export class ShopItemComponent implements OnInit {
   @Input() Product: IProduct;
+  mainImage: string;
+
+  constructor(
+    private _service: BasketService,
+    private favoriteService: FavoriteService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.Product?.photos && this.Product.photos.length > 0) {
+      this.mainImage = 'https://e-com-app-xfqq.onrender.com/' + this.Product.photos[0].imageName;
+    } else {
+      this.mainImage = 'assets/no-image.png';
+    }
+  }
+
+  changeMainImage(img: string) {
+    this.mainImage = 'https://e-com-app-xfqq.onrender.com/' + img;
+  }
+
   SetBasketValue() {
     this._service.addItemToBasket(this.Product);
   }
-  getArrayofRating(rateOfnumber:number):number[]{
-    return Array(rateOfnumber).fill(0).map((x,i)=>i);
-  }
-  
-addToFavorites(productId: number) {
-  const username = 'MRX_17'; // أو خذه من AuthService
-  this.favoriteService.addToFavorites(productId, username).subscribe({
-    next: () => {
-      this.favoriteService.incrementFavoriteCount();
-    },
-    error: err => console.error(err)
-  });
-}
 
+  getArrayofRating(rateOfnumber: number): number[] {
+    return Array(rateOfnumber).fill(0).map((x, i) => i);
+  }
+
+  addToFavorites(productId: number) {
+    const username = 'MRX_17';
+    this.favoriteService.addToFavorites(productId, username).subscribe({
+      next: () => {
+        this.favoriteService.incrementFavoriteCount();
+      },
+      error: (err) => console.error(err)
+    });
+  }
 }
