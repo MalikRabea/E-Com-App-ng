ab80505f9ebf633f455b7b609ddd4bb2366f9bef
Malik <Malik@MALIK-X>
2025-09-11 18:30:40 +0300

    fix: checkout

--- client-ng-shop-main/src/app/checkout/success/success.component.ts
+++ client-ng-shop-main/src/app/checkout/success/success.component.ts
@@ -1,32 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IOrder } from '../../shared/Models/Order';
-import { OrdersService } from '../../orders/orders.service';
 
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
-  styleUrls: ['./success.component.scss']
+  styleUrl: './success.component.scss'
 })
 export class SuccessComponent implements OnInit {
-  orderId: number;
-  order: IOrder | null = null;
-
-  constructor(
-    private route: ActivatedRoute,
-    private ordersService: OrdersService
-  ) {}
-
+  constructor(private route:ActivatedRoute) { }
   ngOnInit(): void {
-    this.orderId = Number(this.route.snapshot.queryParamMap.get('orderId'));
-
-    if (this.orderId) {
-      this.ordersService.getCurrentOrderForUser(this.orderId).subscribe({
-        next: (order) => {
-          this.order = order;
-        },
-        error: (err) => console.error(err)
-      });
-    }
+    this.route.queryParams.subscribe(param=>{
+      this.orderId=param['orderId'];
+    })
   }
+  orderId:number=0;
 }
